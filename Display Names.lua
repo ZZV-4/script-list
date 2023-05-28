d13b4f0a264e3a8b3645dde96d38199e8c5aad99
eyesore <118347384+ZZV-4@users.noreply.github.com>
2023-05-28 21:50:55 +0800

    Update Display Names.lua

--- Display Names.lua
+++ Display Names.lua
@@ -1,3 +1,8 @@
+--[[ Made by eyesore (me) ]]
+-- Have Display Names and Usernames, Great right? Yeah ik.
+
+--//Initialization\\--
+
 for i,lplr in pairs(game:GetService("Players"):GetPlayers()) do
 	lplr.Character.Humanoid.DisplayName = lplr.DisplayName.."\n\@"..lplr.Name
 	lplr.Character.Humanoid.NameDisplayDistance = math.huge
