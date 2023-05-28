160f11f81142983ee6b1898f491919dd5d2b33d2
eyesore <118347384+ZZV-4@users.noreply.github.com>
2023-05-28 21:44:19 +0800

    Add files via upload

---
+++ Display Names.lua
@@ -0,0 +1,20 @@
+for i,lplr in pairs(game:GetService("Players"):GetPlayers()) do
+	lplr.Character.Humanoid.DisplayName = lplr.DisplayName.."\n\@"..lplr.Name
+	lplr.Character.Humanoid.NameDisplayDistance = math.huge
+    lplr.CharacterAdded:Connect(function()
+        lplr.Humanoid.Character:WaitForChild("Humanoid").DisplayName = lplr.DisplayName.."\n\@"..lplr.Name
+		lplr.Character.Humanoid.NameDisplayDistance = math.huge
+    end)
+end
+
+game:GetService("Players").PlayerAdded:Connect(function(lplr)
+    repeat
+		wait()
+	until lplr.Character ~= nil
+	lplr.Character:WaitForChild("Humanoid").DisplayName = lplr.DisplayName.."\n\@"..lplr.Name
+	lplr.Character.Humanoid.NameDisplayDistance = math.huge
+    lplr.CharacterAdded:Connect(function()
+        lplr.Character:WaitForChild("Humanoid").DisplayName = lplr.DisplayName.."\n\@"..lplr.Name
+		lplr.Character.Humanoid.NameDisplayDistance = math.huge
+	end)
+end)
